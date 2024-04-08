18227b9998d41f65ae54a91ccb4807217023bb61
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-04-08 15:24:57 +0900

    ✨[init]

---
+++ websocket-study/src/test/java/org/handmk/websocketstudy/WebsocketStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.handmk.websocketstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
